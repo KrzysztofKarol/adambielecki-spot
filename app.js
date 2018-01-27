ff096c581c8e169b3806bad2f088c139b9b88f38
Krzysztof Karol <krzysztof.karol@gmail.com>
2018-01-27 23:47:40 +0100

    Use real spotURL

--- app.js
+++ app.js
@@ -10,12 +10,11 @@ app.use(express.static('public'));
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
 
-// let spotURL = "https://api.findmespot.com/spot-main-web/consumer/rest-api/2.0/public/feed/0hN62C8mlLHPINqVLLi8qAv2qonI9biev/message.json?limit=10"
-const spotURL = "https://adambielecki-spot.herokuapp.com/dataTest.json";
+let spotURL = "https://api.findmespot.com/spot-main-web/consumer/rest-api/2.0/public/feed/0hN62C8mlLHPINqVLLi8qAv2qonI9biev/message.json?limit=10"
 const googleElevation = "https://maps.googleapis.com/maps/api/elevation/json?key=AIzaSyCaatjrKpdC3wvr_5AFLmA-ssobawWVAmo";
 
 updateData();
-new CronJob('*/5 * * * * *', () => {
+new CronJob('0 */5 * * * *', () => {
   updateData();
 }, null, true, 'Europe/Warsaw');
 
